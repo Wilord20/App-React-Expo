9c65a92f2b61ab3596590aeaafee1e119ba28c61
Wilord20 <edgartorres08@live.com.mx>
2024-11-12 16:30:37 -0600

    Creating Imageviewer and Button component

--- StickerSmash/app/(tabs)/index.tsx
+++ StickerSmash/app/(tabs)/index.tsx
@@ -1,21 +1,21 @@
-import { Text, View, StyleSheet } from "react-native";
-import { Image } from "expo-image";
+import { View, StyleSheet } from "react-native";
+import ImageViewer from "@/components/ImageViewer";
+import Button from "@/components/Button";
 
 const PlaceholderImage = require("@/assets/images/background-image.png");
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#25292e',
-    alignItems: 'center',
+    backgroundColor: "#25292e",
+    alignItems: "center",
   },
   imageContainer: {
     flex: 1,
   },
-   image: {
-    width: 320,
-    height: 440,
-    borderRadius: 18,
+  footerContainer: {
+    flex: 1 / 4,
+    alignItems: "center",
   },
 });
 
@@ -23,7 +23,11 @@ export default function Index() {
   return (
     <View style={styles.container}>
       <View style={styles.imageContainer}>
-        <Image source={PlaceholderImage} style={styles.image} />
+        <ImageViewer imgSource={PlaceholderImage} />
+      </View>
+      <View style={styles.footerContainer}>
+        <Button label="Elige una foto" theme="primary" />
+        <Button label="Usar esta foto" />
       </View>
     </View>
   );
