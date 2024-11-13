c7d736f5dce26f2a069aedc111f3778cd1f62a9d
Wilord20 <edgartorres08@live.com.mx>
2024-11-12 18:07:21 -0600

    Implementing Logic to ImageViewer and SelectedImage

--- StickerSmash/app/(tabs)/index.tsx
+++ StickerSmash/app/(tabs)/index.tsx
@@ -1,8 +1,38 @@
 import { View, StyleSheet } from "react-native";
 import ImageViewer from "@/components/ImageViewer";
 import Button from "@/components/Button";
+import * as ImagePicker from "expo-image-picker";
+import { useState } from "react";
 
 const PlaceholderImage = require("@/assets/images/background-image.png");
+
+export default function Index() {
+  const [selectedImage, setSelectedImage] = useState<string | undefined>(undefined);
+
+  const pickImageAsync = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      allowsEditing: true,
+      quality: 1,
+    });
+
+    if (!result.canceled) {
+      setSelectedImage(result.assets[0].uri);
+    } else {
+      console.log("No se selecciono ninguna imagen.");
+    }
+  };
+  return (
+    <View style={styles.container}>
+      <View style={styles.imageContainer}>
+        <ImageViewer imgSource={PlaceholderImage} selectedImage={selectedImage}/>
+      </View>
+      <View style={styles.footerContainer}>
+        <Button label="Elige una foto" theme="primary" onPress={pickImageAsync}/>
+        <Button label="Usar esta foto" />
+      </View>
+    </View>
+  );
+}
 
 const styles = StyleSheet.create({
   container: {
@@ -18,17 +48,3 @@ const styles = StyleSheet.create({
     alignItems: "center",
   },
 });
-
-export default function Index() {
-  return (
-    <View style={styles.container}>
-      <View style={styles.imageContainer}>
-        <ImageViewer imgSource={PlaceholderImage} />
-      </View>
-      <View style={styles.footerContainer}>
-        <Button label="Elige una foto" theme="primary" />
-        <Button label="Usar esta foto" />
-      </View>
-    </View>
-  );
-}
--- StickerSmash/components/Button.tsx
+++ StickerSmash/components/Button.tsx
@@ -1,38 +1,50 @@
 import { StyleSheet, View, Pressable, Text } from "react-native";
-import FontAwesome from '@expo/vector-icons/FontAwesome';
+import FontAwesome from "@expo/vector-icons/FontAwesome";
 
 type Props = {
   label: string;
-  theme? : 'primary';
+  theme?: "primary";
+  onPress?: () => void;
 };
 
-
-export default function Button({ label, theme }: Props) {
-    if (theme === 'primary') {
-      return (
-        <View
-          style={[
-            styles.buttonContainer,
-            { borderWidth: 4, borderColor: '#ffd33d', borderRadius: 18 },
-          ]}>
-          <Pressable
-            style={[styles.button, { backgroundColor: '#fff' }]}
-            onPress={() => alert('Has presionado un boton.')}>
-            <FontAwesome name="picture-o" size={18} color="#25292e" style={styles.buttonIcon} />
-            <Text style={[styles.buttonLabel, { color: '#25292e' }]}>{label}</Text>
-          </Pressable>
-        </View>
-      );
-    }
-  
+export default function Button({ label, theme, onPress }: Props) {
+  if (theme === "primary") {
     return (
-      <View style={styles.buttonContainer}>
-        <Pressable style={styles.button} onPress={() => alert('Has presionado un boton')}>
-          <Text style={styles.buttonLabel}>{label}</Text>
+      <View
+        style={[
+          styles.buttonContainer,
+          { borderWidth: 4, borderColor: "#ffd33d", borderRadius: 18 },
+        ]}
+      >
+        <Pressable
+          style={[styles.button, { backgroundColor: "#fff" }]}
+          onPress={onPress}
+        >
+          <FontAwesome
+            name="picture-o"
+            size={18}
+            color="#25292e"
+            style={styles.buttonIcon}
+          />
+          <Text style={[styles.buttonLabel, { color: "#25292e" }]}>
+            {label}
+          </Text>
         </Pressable>
       </View>
     );
   }
+
+  return (
+    <View style={styles.buttonContainer}>
+      <Pressable
+        style={styles.button}
+        onPress={onPress}
+      >
+        <Text style={styles.buttonLabel}>{label}</Text>
+      </Pressable>
+    </View>
+  );
+}
 
 const styles = StyleSheet.create({
   buttonContainer: {
